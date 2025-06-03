16a97fc6844ecd45f37fd0acbcc9657defc1334a
Masoud-ms8 <masoudsaberi10@gmail.com>
2025-06-03 14:14:32 +0330

    new

---
+++ script.js
@@ -0,0 +1,6 @@
+        function masoud(){
+            document.getElementsByTagName("h1")[1].innerHTML="hi masoud";
+        }
+        // function masoud(){
+        //     alert("salam")
+        // }
