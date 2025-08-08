d6c78a0f1c7c6453b193eccd1e6b2ebb0cc5ba96
Masoud-ms8 <masoudsaberi10@gmail.com>
2025-08-09 00:05:54 +0330

    1

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
             document.getElementsByTagName("h1")[1].innerHTML="hi masoud";
         }
 
-        function masoud(){
-            alert("salam")
-        }
+        // function masoud(){
+        //     alert("salam")
+        // }
         
-        document.getElementById("pm").innerHTML= 10+5;
+        // document.getElementById("pm").innerHTML= 10+5;
 
-        var name = 'salam khobi?'
-        alert("my pm :"+name)
+        // var name = 'salam khobi?'
+        // alert("my pm :"+name)
 
         // var age = prompt("enter your age:")
         // age = Number(age)
