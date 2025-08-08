1d301edb92bf4a047303be62bb1819717306c0ef
Masoud-ms8 <masoudsaberi10@gmail.com>
2025-08-09 00:04:54 +0330

    1

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-        // function masoud(){
-        //     document.getElementsByTagName("h1")[1].innerHTML="hi masoud";
-        // }
-
-        // function masoud(){
-        //     alert("salam")
-        // }
-        
-        // document.getElementById("pm").innerHTML= 10+5;
-
-        // var name = 'salam khobi?'
-        // alert("my pm :"+name)
-
-        var age = prompt("enter your age:")
-        age = Number(age)
-
-        if(age > 40){
-            alert("oh oh dari pir mishiaaa")
-        }else{
-            alert("hanoz javoni jojo")
+        function masoud(){
+            document.getElementsByTagName("h1")[1].innerHTML="hi masoud";
         }
+
+        function masoud(){
+            alert("salam")
+        }
+        
+        document.getElementById("pm").innerHTML= 10+5;
+
+        var name = 'salam khobi?'
+        alert("my pm :"+name)
+
+        // var age = prompt("enter your age:")
+        // age = Number(age)
+
+        // if(age > 40){
+        //     alert("oh oh dari pir mishiaaa")
+        // }else{
+        //     alert("hanoz javoni jojo")
+        // }
